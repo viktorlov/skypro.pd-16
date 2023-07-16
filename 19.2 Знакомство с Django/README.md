ff03c2905dc928707d1552899a55e4c7f98d56de
viktorlov <108744561+viktorlov@users.noreply.github.com>
2023-07-16 18:54:02 +0300

    Delete 19.2 Знакомство с Django directory

--- 19.2 Знакомство с Django/README.md
+++
@@ -1,1 +0,0 @@
-https://my.sky.pro/student-cabinet/stream-lesson/84545
--- 19.2 Знакомство с Django/catalog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from catalog.views import index, contacts
-
-urlpatterns = [
-    path('', index),
-    path('contacts/', contacts)
-]
